92b4b096d21b9dcaf03e1e68e29364a55822e1e6
Esjihn <sysnom@gmail.com>
2022-06-05 10:36:38 -0400

    adding cors policy annotations for local host to web api

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -2,6 +2,7 @@ package com.luv2code.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
@@ -9,5 +10,4 @@ public class SpringBootEcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
 	}
-
 }
